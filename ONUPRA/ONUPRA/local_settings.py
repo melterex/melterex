26beb4b815d3e979da38cae2decc731f46345d2e
melterex <melt.t.aleksandrov@gmail.com>
2023-04-14 09:43:53 +0700

    12222

--- ONUPRA/ONUPRA/local_settings.py
+++ ONUPRA/ONUPRA/local_settings.py
@@ -3,7 +3,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -13,9 +12,9 @@ SECRET_KEY = 'django-insecure-48cu=1(5ln)ydmpn-%%e)0@*u+wel*tmfhdn59uss&6^_1$gj7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['onupra.ru', '127.0.0.1']
+ALLOWED_HOSTS = ['onupra.ru', '192.168.17.15', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = [
-    'https://onupra.ru', 'http://127.0.0.1:8000']
+    'https://onupra.ru', 'http://127.0.0.1:8000', 'http://onupra.ru', 'https://192.168.17.15:8000']
 
 DATABASES = {
     'default': {
@@ -24,6 +23,8 @@ DATABASES = {
     }
 }
 
-STATICFILES_DIRS = [
-    BASE_DIR / "main/static/",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'main/static/')
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "main/static/",
+# ]
